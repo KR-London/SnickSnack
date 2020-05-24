eaa0addf3a52d4f12bc47766ad6290bc8a47196d
kate <ekaterina.roberts@gmail.com>
2020-05-24 07:00:41 +0100

    Initial Commit

---
+++ SnickSnack/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SnickSnack
+//
+//  Created by Kate Roberts on 24/05/2020.
+//  Copyright © 2020 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnickSnack/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SnickSnack
+//
+//  Created by Kate Roberts on 24/05/2020.
+//  Copyright © 2020 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
